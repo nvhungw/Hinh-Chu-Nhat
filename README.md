c16841a71cb46b59c58a392c05eda8a3ee067458
nvhungw <119866957+nvhungw@users.noreply.github.com>
2023-05-28 21:53:47 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Hinh-Chu-Nhat
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-qfkcrc)
---
+++ script.js
@@ -0,0 +1,32 @@
+console.log('hello');
+class Rectangle {
+  constructor(widthInit, heightInit) {
+    this.width = widthInit;
+    this.height = heightInit;
+    this.xStart = 0;
+    this.yStart = 0;
+  }
+
+  getArea() {
+    return this.width * this.height;
+  }
+
+  getPerimeter() {
+    return (this.width + this.height) * 2;
+  }
+
+  draw() {
+    let board = document.getElementById('board');
+    let context = board.getContext('2d');
+    context.fillStyle = 'blue';
+    context.fillRect(this.xStart, this.yStart, this.width, this.height);
+  }
+}
+
+let rectangleObj = new Rectangle(200, 100);
+// rectangleObj.draw()
+console.log(rectangleObj);
+console.log('dien tich:' + rectangleObj.getArea());
+
+console.log('chuvi:' + rectangleObj.getPerimeter());
+let board = document.getElementById('board');
